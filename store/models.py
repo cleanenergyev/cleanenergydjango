7d20c61e0c0d1a3be962a80bc1165ed1ef5fb09a
Sangram <sangramsahutech@gmail.com>
2022-06-24 15:19:40 +0530

    sangram/dev2 bike stock

--- store/models.py
+++ store/models.py
@@ -51,6 +51,148 @@ class Product(models.Model):
         return self.name
 
 
+class Accessories(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    accessoriescode = models.PositiveIntegerField(unique=True)
+    mrp = models.PositiveIntegerField()
+    gst = models.PositiveIntegerField()
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Battery(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class VehicleModel(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ChargerRating(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ToolkitStatus(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class RvmLhrh(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CertCard(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PdiStatus(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class BatteryMake(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class BatteryConnectorType(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class BatteryType(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class BatteryCapacity(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class VehicleStock(models.Model):
+
+    qrcode = models.CharField(max_length=120, unique=True)
+    productcode = models.PositiveIntegerField()
+    productdesc = models.CharField(max_length=120)
+    vehicleinvoiceno = models.CharField(max_length=120)
+    vehicleinvoicedatefromhe = models.DateField()
+    exciseinvoiceno = models.CharField(max_length=120)
+    battery = models.ForeignKey(Battery, on_delete=models.CASCADE)
+    model = models.ForeignKey(VehicleModel, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    vehiclechassisno = models.CharField(max_length=120)
+    motorno = models.CharField(max_length=120)
+    controllerno = models.CharField(max_length=120)
+    chargerno = models.CharField(max_length=120)
+    chargerrating = models.ForeignKey(ChargerRating, on_delete=models.CASCADE)
+    ownersmanualno = models.CharField(max_length=120)
+    heronumberplate = models.CharField(max_length=20, default='NA', blank=True, null=True)
+    toolkit = models.ForeignKey(ToolkitStatus, on_delete=models.CASCADE)
+    rvmlhrh = models.ForeignKey(RvmLhrh, on_delete=models.CASCADE)
+    certcard = models.ForeignKey(CertCard, on_delete=models.CASCADE)
+    pdistatus = models.ForeignKey(PdiStatus, on_delete=models.CASCADE)
+    batterymake = models.ForeignKey(BatteryMake, on_delete=models.CASCADE)
+    batteryconnectortype = models.ForeignKey(BatteryConnectorType, on_delete=models.CASCADE)
+    engravednumberonbattery = models.CharField(max_length=50, default='NA', blank=True, null=True)
+    typeofbattery = models.ForeignKey(BatteryType, on_delete=models.CASCADE)
+    batterycapacity = models.ForeignKey(BatteryCapacity, on_delete=models.CASCADE)
+    batterycompleteserialno = models.CharField(max_length=50, blank=True, null=True)
+    available = models.BooleanField(default=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.qrcode
+
+
 class Order(models.Model):
     STATUS_CHOICE = (
         ('pending', 'Pending'),
@@ -74,6 +216,13 @@ class Order(models.Model):
     def __str__(self):
         return self.product.name
 
+
+class VehicleStockDetails(models.Model):
+    productcode = models.ForeignKey(VehicleStock, on_delete=models.CASCADE)
+    productdesc = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.productdesc
 
 class Delivery(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count, Q
 from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
@@ -10,7 +11,21 @@ from .models import (
     Drop,
     Product,
     Order,
-    Delivery
+    Delivery,
+    Accessories,
+    VehicleStock,
+    Battery,
+    VehicleModel,
+    Color,
+    ChargerRating,
+    ToolkitStatus,
+    RvmLhrh,
+    CertCard,
+    PdiStatus,
+    BatteryMake,
+    BatteryConnectorType,
+    BatteryType,
+    BatteryCapacity
 )
 from .forms import (
     SupplierForm,
@@ -19,7 +34,21 @@ from .forms import (
     DropForm,
     ProductForm,
     OrderForm,
-    DeliveryForm
+    DeliveryForm,
+    AccessoriesForm,
+    VehicleStockForm,
+    BatteriesForm,
+    VehicleModelForm,
+    ColorForm,
+    ChargerRatingForm,
+    ToolkitStatusForm,
+    RvmLhrhForm,
+    CertCardForm,
+    PdiStatusForm,
+    BatteryMakeForm,
+    BatteryConnectorTypeForm,
+    BatteryTypeForm,
+    BatteryCapacityForm
 )
 
 # Supplier views
@@ -142,6 +171,106 @@ class ProductListView(ListView):
     context_object_name = 'product'
 
 
+@login_required(login_url='login')
+def create_accessories(request):
+    forms = AccessoriesForm()
+    if request.method == 'POST':
+        forms = AccessoriesForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('accessories-list')
+    context = {
+        'form': forms
+    }
+    return render(request, 'store/addAccessories.html', context)
+
+
+class AccessoriesListView(ListView):
+    model = Accessories
+    template_name = 'store/accessories_list.html'
+    context_object_name = 'accessories'
+
+
+@login_required(login_url='login')
+def create_vehicle_stock(request):
+    forms = VehicleStockForm()
+    if request.method == 'POST':
+        forms = VehicleStockForm(request.POST)
+        if forms.is_valid():
+            # forms.save()
+            qrcode = forms.cleaned_data['qrcode']
+            vehicleinvoiceno = forms.cleaned_data['vehicleinvoiceno']
+            vehicleinvoicedatefromhe = forms.cleaned_data['vehicleinvoicedatefromhe']
+            exciseinvoiceno = forms.cleaned_data['exciseinvoiceno']
+            battery = forms.cleaned_data['battery']
+            model = forms.cleaned_data['model']
+            color = forms.cleaned_data['color']
+            vehiclechassisno = forms.cleaned_data['vehiclechassisno']
+            motorno = forms.cleaned_data['motorno']
+            controllerno = forms.cleaned_data['controllerno']
+            chargerno = forms.cleaned_data['chargerno']
+            chargerrating = forms.cleaned_data['chargerrating']
+            ownersmanualno = forms.cleaned_data['ownersmanualno']
+            heronumberplate = forms.cleaned_data['heronumberplate']
+            toolkit = forms.cleaned_data['toolkit']
+            rvmlhrh = forms.cleaned_data['rvmlhrh']
+            certcard = forms.cleaned_data['certcard']
+            pdistatus = forms.cleaned_data['pdistatus']
+            batterymake = forms.cleaned_data['batterymake']
+            batteryconnectortype = forms.cleaned_data['batteryconnectortype']
+            engravednumberonbattery = forms.cleaned_data['engravednumberonbattery']
+            typeofbattery = forms.cleaned_data['typeofbattery']
+            batterycapacity = forms.cleaned_data['batterycapacity']
+            batterycompleteserialno = forms.cleaned_data['batterycompleteserialno']
+            available = True
+            productdesctest = Product.objects.filter(productcode=qrcode.split('A')[0])
+            print(productdesctest)
+            VehicleStock.objects.create(
+                qrcode=qrcode,
+                productcode=qrcode.split('A')[0],
+                productdesc=productdesctest[0],
+                vehicleinvoiceno=vehicleinvoiceno,
+                vehicleinvoicedatefromhe=vehicleinvoicedatefromhe,
+                exciseinvoiceno=exciseinvoiceno,
+                battery=battery,
+                model=model,
+                color=color,
+                vehiclechassisno=vehiclechassisno,
+                motorno=motorno,
+                controllerno=controllerno,
+                chargerno=chargerno,
+                chargerrating=chargerrating,
+                ownersmanualno=ownersmanualno,
+                heronumberplate=heronumberplate,
+                toolkit=toolkit,
+                rvmlhrh=rvmlhrh,
+                certcard=certcard,
+                pdistatus=pdistatus,
+                batterymake=batterymake,
+                batteryconnectortype=batteryconnectortype,
+                engravednumberonbattery=engravednumberonbattery,
+                typeofbattery=typeofbattery,
+                batterycapacity=batterycapacity,
+                batterycompleteserialno=batterycompleteserialno,
+                available=available
+            )
+            return redirect('vehicle-list')
+    context = {
+        'form': forms
+    }
+    return render(request, 'store/addStock.html', context)
+
+
+class VehicleListView(ListView):
+    model = VehicleStock
+    template_name = 'store/vehicle_stock_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['vehiclestock'] = VehicleStock.objects.all().order_by('-id')
+        return context
+
+
 # Order views
 @login_required(login_url='login')
 def create_order(request):
@@ -202,3 +331,260 @@ class DeliveryListView(ListView):
     model = Delivery
     template_name = 'store/delivery_list.html'
     context_object_name = 'delivery'
+
+
+@login_required(login_url='login')
+def create_battery(request):
+    forms = BatteriesForm()
+    if request.method == 'POST':
+        forms = BatteriesForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('battery-list')
+    context = {
+        'form': forms
+    }
+    return render(request, 'store/addBattery.html', context)
+
+
+class BatteriesListView(ListView):
+    model = Battery
+    template_name = 'store/batteries_list.html'
+    context_object_name = 'batteries'
+
+
+@login_required(login_url='login')
+def create_vehicle_model(request):
+    forms = VehicleModelForm()
+    if request.method == 'POST':
+        forms = VehicleModelForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('vehicle-model-list')
+    context = {
+        'form': forms
+    }
+    return render(request, 'store/addVehicleModel.html', context)
+
+
+class VehicleModelListView(ListView):
+    model = VehicleModel
+    template_name = 'store/vehicle_model_list.html'
+    context_object_name = 'vehiclemodel'
+
+
+@login_required(login_url='login')
+def create_color(request):
+    forms = ColorForm()
+    if request.method == 'POST':
+        forms = ColorForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('color-list')
+    context = {
+        'form': forms
+    }
+    return render(request, 'store/addColor.html', context)
+
+
+class ColorListView(ListView):
+    model = Color
+    template_name = 'store/color_list.html'
+    context_object_name = 'color'
+
+
+@login_required(login_url='login')
+def create_charger_rating(request):
+    forms = ChargerRatingForm()
+    if request.method == 'POST':
+        forms = ChargerRatingForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('charger-rating-list')
+    context = {
+        'form': forms
+    }
+    return render(request, 'store/addChargerRating.html', context)
+
+
+class ChargerRatingListView(ListView):
+    model = ChargerRating
+    template_name = 'store/charger_rating_list.html'
+    context_object_name = 'chargerrating'
+
+
+@login_required(login_url='login')
+def create_toolkit_status(request):
+    forms = ToolkitStatusForm()
+    if request.method == 'POST':
+        forms = ToolkitStatusForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('toolkit-status-list')
+    context = {
+        'form': forms
+    }
+    return render(request, 'store/addToolkitStatus.html', context)
+
+
+class ToolkitStatusListView(ListView):
+    model = ToolkitStatus
+    template_name = 'store/toolkit_status_list.html'
+    context_object_name = 'toolkitstatus'
+
+
+@login_required(login_url='login')
+def create_rvm_lhrh(request):
+    forms = RvmLhrhForm()
+    if request.method == 'POST':
+        forms = RvmLhrhForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('rvm-lhrh-list')
+    context = {
+        'form': forms
+    }
+    return render(request, 'store/addRvmlhrh.html', context)
+
+
+class RvmLhrhListView(ListView):
+    model = RvmLhrh
+    template_name = 'store/rvm_lhrh_list.html'
+    context_object_name = 'rvmlhrh'
+
+
+@login_required(login_url='login')
+def create_cert_card(request):
+    forms = CertCardForm()
+    if request.method == 'POST':
+        forms = CertCardForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('cert-card-list')
+    context = {
+        'form': forms
+    }
+    return render(request, 'store/addCertCard.html', context)
+
+
+class CertCardListView(ListView):
+    model = CertCard
+    template_name = 'store/cert_card_list.html'
+    context_object_name = 'certcard'
+
+
+
+@login_required(login_url='login')
+def create_pdi_status(request):
+    forms = PdiStatusForm()
+    if request.method == 'POST':
+        forms = PdiStatusForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('pdi-status-list')
+    context = {
+        'form': forms
+    }
+    return render(request, 'store/addPdiStatus.html', context)
+
+
+class PdiStatusListView(ListView):
+    model = PdiStatus
+    template_name = 'store/pdi_status_list.html'
+    context_object_name = 'pdistatus'
+
+
+@login_required(login_url='login')
+def create_battery_make(request):
+    forms = BatteryMakeForm()
+    if request.method == 'POST':
+        forms = BatteryMakeForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('battery-make-list')
+    context = {
+        'form': forms
+    }
+    return render(request, 'store/addBatteryMake.html', context)
+
+
+class BatteryMakeListView(ListView):
+    model = BatteryMake
+    template_name = 'store/battery_make_list.html'
+    context_object_name = 'batterymake'
+
+
+@login_required(login_url='login')
+def create_battery_connector_type(request):
+    forms = BatteryConnectorTypeForm()
+    if request.method == 'POST':
+        forms = BatteryConnectorTypeForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('battery-connector-type-list')
+    context = {
+        'form': forms
+    }
+    return render(request, 'store/addBatteryConnectorType.html', context)
+
+
+class BatteryConnectorTypeListView(ListView):
+    model = BatteryConnectorType
+    template_name = 'store/battery_connector_type_list.html'
+    context_object_name = 'batteryconnectortype'
+
+
+@login_required(login_url='login')
+def create_battery_type(request):
+    forms = BatteryTypeForm()
+    if request.method == 'POST':
+        forms = BatteryTypeForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('battery-type-list')
+    context = {
+        'form': forms
+    }
+    return render(request, 'store/addBatteryType.html', context)
+
+
+class BatteryTypeListView(ListView):
+    model = BatteryType
+    template_name = 'store/battery_type_list.html'
+    context_object_name = 'batterytype'
+
+
+@login_required(login_url='login')
+def create_battery_capacity(request):
+    forms = BatteryCapacityForm()
+    if request.method == 'POST':
+        forms = BatteryCapacityForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('battery-capacity-list')
+    context = {
+        'form': forms
+    }
+    return render(request, 'store/addBatteryCapacity.html', context)
+
+
+class BatteryCapacityListView(ListView):
+    model = BatteryCapacity
+    template_name = 'store/battery_capacity_list.html'
+    context_object_name = 'batterycapacity'
+
+
+class SearchResultsView(ListView):
+    model = VehicleStock
+    template_name = 'store/search_results.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        query = self.request.GET.get('q')
+        context['x'] = VehicleStock.objects.all().order_by
+        print(context)
+        print(VehicleStock.objects.values_list('productcode', 'color', 'model', 'productdesc'))
+        context['vehiclestockdetails'] = VehicleStock.objects.values('productcode', 'color', 'model', 'productdesc') \
+            .annotate(total=Count('color')) \
+            .filter(Q(productdesc__icontains=query) & Q(available=True))
+        return context
