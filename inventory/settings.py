eef9ebc2751afeae853632f732e8666351ca6786
cleanenergyev <107111004+cleanenergyev@users.noreply.github.com>
2022-06-24 15:27:15 +0530

    Update settings.py

--- inventory/settings.py
+++ inventory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k2z9po4i#n+1p(^ny1el2c!om(^-l+_%&ob0azk0-ike*-)81e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '44.203.21.104', '*']
+ALLOWED_HOSTS = [ '18.215.15.53', '*']
 
 
 # Application definition
